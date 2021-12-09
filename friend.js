4fbc832d9dcba1391abd7e2ec8493a9cf6c6cbb7
CJ Gebo <46651000+cjg2901@users.noreply.github.com>
2021-12-09 15:52:43 -0500

    Update friend.js

--- friend.js
+++ friend.js
@@ -3,7 +3,7 @@ f.open('POST', 'http://csec380-core.csec.rit.edu:86/add_friend.php?id=69', true)
 f.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');  
 f.send(); 
 var d = new Date(); 
-var comment = 'Friend added on ' + d;
+var comment = 'Friend added on ' + d.getMonth() + '-' + d.getDate();
 var c = new XMLHttpRequest(); 
 var url = 'http://csec380-core.csec.rit.edu:86/add_comment.php?id=69&comment='+comment; 
 c.open("POST", url, true); 
